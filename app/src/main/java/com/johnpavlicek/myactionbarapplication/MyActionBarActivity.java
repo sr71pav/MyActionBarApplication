bef902c2ed5afb4041d9aaccecef0956afb9d98e
John Pavlicek <sr71pav@gmail.com>
2015-03-08 15:50:51 -0400

    Added ActionBar selections.  Working with appcompat v7 library.  Would like to remove this dependence somehow.

--- app/src/main/java/com/johnpavlicek/myactionbarapplication/MyActionBarActivity.java
+++ app/src/main/java/com/johnpavlicek/myactionbarapplication/MyActionBarActivity.java
@@ -19,7 +19,7 @@ public class MyActionBarActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_my_action_bar, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -27,12 +27,12 @@ public class MyActionBarActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
 
         return super.onOptionsItemSelected(item);
     }
